e9b0ce0bc0f057877d9a6d86619782db1c4b79ea
Gustavo Trindade <gustavoferreiratrindade@gmail.com>
2023-02-10 09:46:14 -0300

    feat(webapp): novas implementacoes de view

--- src/web/NSE.WebApp.MVC/Program.cs
+++ src/web/NSE.WebApp.MVC/Program.cs
@@ -1,27 +1,16 @@
+using NSE.WebApp.MVC.Configuration;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddIdentityConfiguration();
+builder.Services.AddMvcConfiguration();
 
 var app = builder.Build();
+app.UseMvcConfiguration();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+await app.RunAsync();
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+//Fazer commit e criar classe startup
